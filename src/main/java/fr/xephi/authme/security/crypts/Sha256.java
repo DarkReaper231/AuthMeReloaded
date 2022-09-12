95c4b55da8d6ce2a77fa9f3d94f7a6162e66f3b1
DarkReaper231 <blackcreepercz012345@gmail.com>
2022-09-12 21:35:23 +0200

    sus

--- src/main/java/fr/xephi/authme/security/crypts/Sha256.java
+++ src/main/java/fr/xephi/authme/security/crypts/Sha256.java
@@ -3,15 +3,18 @@ package fr.xephi.authme.security.crypts;
 import fr.xephi.authme.security.crypts.description.Recommendation;
 import fr.xephi.authme.security.crypts.description.Usage;
 
+import java.util.Base64;
+
 import static fr.xephi.authme.security.HashUtils.isEqual;
-import static fr.xephi.authme.security.HashUtils.sha256;
 
 @Recommendation(Usage.RECOMMENDED)
 public class Sha256 extends HexSaltedMethod {
 
     @Override
     public String computeHash(String password, String salt, String name) {
-        return "$SHA$" + salt + "$" + sha256(sha256(password) + salt);
+        //return "$SHA$" + salt + "$" + sha256(sha256(password) + salt);
+        String preHash = password + "_" + salt;
+        return "$SHA$" + salt + "$" + Base64.getEncoder().encodeToString(preHash.getBytes());
     }
 
     @Override
